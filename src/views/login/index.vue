33208ad6882d05bd42cc02f673657af91b7d9762
董宇成 <dongyucheng@unioncast.cn>
2022-02-08 14:51:33 +0800

    调整登录页面样式

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,72 +1,76 @@
 <template>
   <div class="login-container">
-    <el-form
-      ref="dataFrom"
-      :model="dataFrom"
-      :rules="dataFromRules"
-      class="login-form"
-      label-position="left"
-    >
-      <div class="title-container">
-        <h3 class="title">{{ title }}</h3>
-      </div>
-      <!-- 用户名 -->
-      <el-form-item prop="username">
-        <span class="svg-container">
-          <svg-icon icon-class="user" />
-        </span>
-        <el-input v-model.trim="dataFrom.username" type="text" trim />
-      </el-form-item>
-      <!-- 密码 -->
-      <el-form-item prop="password">
-        <span class="svg-container">
-          <svg-icon icon-class="password" />
-        </span>
-        <el-input
-          :key="passwordType"
-          ref="password"
-          v-model="dataFrom.password"
-          :type="passwordType"
-          placeholder=""
-          name="password"
-          @keyup.enter.native="handleLogin"
-        />
-        <span class="show-pwd" @click="showPwd">
-          <svg-icon
-            :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'"
-          />
-        </span>
-      </el-form-item>
-      <!-- 验证码 -->
-      <el-form-item>
-        <el-row :gutter="15">
-          <el-col :span="14">
-            <el-input
-              v-model="dataFrom.code"
-              type="text"
-              @keyup.enter.native="handleLogin"
-            />
-          </el-col>
-          <el-col :span="10">
-            <div class="veriCodeImg" @click="getCode">
-              <img :src="codeUrl" />
-            </div>
-          </el-col>
-        </el-row>
-      </el-form-item>
-      <el-button
-        :loading="loading"
-        type="primary"
-        style="width: 100%; margin-bottom: 30px"
-        @click.native.prevent="handleLogin"
+    <div class="top"></div>
+    <div class="bottom"></div>
+    <div class="center">
+      <el-form
+        ref="dataFrom"
+        :model="dataFrom"
+        :rules="dataFromRules"
+        class="login-form"
+        label-position="left"
       >
-        登 录
-      </el-button>
-      <div class="tips">
-        <span style="margin-right: 20px">username: admin</span>
-        <span> password: admin123</span>
-      </div>
-    </el-form>
+        <div class="title-container">
+          <h3 class="title">{{ title }}</h3>
+        </div>
+        <!-- 用户名 -->
+        <el-form-item prop="username">
+          <span class="svg-container">
+            <svg-icon icon-class="user" />
+          </span>
+          <el-input v-model.trim="dataFrom.username" type="text" trim />
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <span class="svg-container">
+            <svg-icon icon-class="password" />
+          </span>
+          <el-input
+            :key="passwordType"
+            ref="password"
+            v-model="dataFrom.password"
+            :type="passwordType"
+            placeholder=""
+            name="password"
+            @keyup.enter.native="handleLogin"
+          />
+          <span class="show-pwd" @click="showPwd">
+            <svg-icon
+              :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'"
+            />
+          </span>
+        </el-form-item>
+        <!-- 验证码 -->
+        <el-form-item>
+          <el-row :gutter="15">
+            <el-col :span="14">
+              <el-input
+                v-model="dataFrom.code"
+                type="text"
+                @keyup.enter.native="handleLogin"
+              />
+            </el-col>
+            <el-col :span="10">
+              <div class="veriCodeImg" @click="getCode">
+                <img :src="codeUrl" />
+              </div>
+            </el-col>
+          </el-row>
+        </el-form-item>
+        <el-button
+          class="login-btn"
+          :loading="loading"
+          type="primary"
+          @click.native.prevent="handleLogin"
+        >
+          登 录
+        </el-button>
+        <div class="tips">
+          <span class="mr-2">用户名: admin</span>
+          <span> 密码: admin123</span>
+        </div>
+      </el-form>
+    </div>
   </div>
 </template>
 
@@ -172,11 +176,83 @@ export default {
 };
 </script>
 
+<style lang="scss" scoped>
+$width: 200vmax;
+$height: 200vmax;
+$excursion_top: 50%;
+$excursion_left: 50%;
+/* 背景样式 */
+.login-container {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  &:hover,
+  &:active {
+    .top,
+    .bottom {
+      &:before,
+      &:after {
+        margin-left: 200px;
+        transform-origin: -200px 50%;
+        transition-delay: 0s;
+      }
+    }
+
+    .center {
+      opacity: 1;
+      transition-delay: 0.2s;
+    }
+  }
+}
+
+.top,
+.bottom {
+  &:before,
+  &:after {
+    content: "";
+    display: block;
+    position: absolute;
+    width: $width;
+    height: $height;
+    top: $excursion_top;
+    left: $excursion_left;
+    margin-top: -100vmax;
+    transform-origin: 0 50%;
+    transition: all 0.5s cubic-bezier(0.445, 0.05, 0, 1);
+    z-index: 10;
+    opacity: 0.65;
+    transition-delay: 0.2s;
+  }
+}
+
+.top {
+  &:before {
+    transform: rotate(45deg);
+    background: #e46569;
+  }
+  &:after {
+    transform: rotate(135deg);
+    background: #ecaf81;
+  }
+}
+
+.bottom {
+  &:before {
+    transform: rotate(-45deg);
+    background: #60b8d4;
+  }
+  &:after {
+    transform: rotate(-135deg);
+    background: #3745b5;
+  }
+}
+</style>
+
 <style lang="scss">
 /* 修复input 背景不协调 和光标变色 */
-$bg: #283443;
-$light_gray: #fff;
-$cursor: #fff;
+$light_gray: rgba(0, 0, 0.85);
+$cursor: rgba(0, 0, 0.85);
 @supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
   .login-container .el-input input {
     color: $cursor;
@@ -186,7 +262,7 @@ $cursor: #fff;
 .login-container {
   .el-input {
     display: inline-block;
-    height: 47px;
+
     width: 85%;
     input {
       background: transparent;
@@ -195,52 +271,60 @@ $cursor: #fff;
       border-radius: 0px;
       padding: 12px 5px 12px 15px;
       color: $light_gray;
-      height: 47px;
       caret-color: $cursor;
-      &:-webkit-autofill {
-        box-shadow: 0 0 0px 1000px $bg inset !important;
-        -webkit-text-fill-color: $cursor !important;
-      }
+      // &:-webkit-autofill {
+      //   box-shadow: 0 0 0px 1000px $bg inset !important;
+      //   -webkit-text-fill-color: $cursor !important;
+      // }
     }
   }
   .el-form-item {
-    border: 1px solid rgba(255, 255, 255, 0.1);
-    background: rgba(0, 0, 0, 0.1);
+    border: 1px solid #dcdfe6;
     border-radius: 5px;
-    color: #454545;
+    color: $light_gray;
+    .el-form-item__content {
+      line-height: 36px;
+    }
   }
 }
 </style>
 
 <style lang="scss" scoped>
-$bg: #2d3a4b;
-$dark_gray: #889aa4;
-$light_gray: #eee;
-.login-container {
-  min-height: 100%;
-  width: 100%;
-  background-color: $bg;
-  overflow: hidden;
+$dark_gray: rgba(0, 0, 0, 0.85);
+$excursion_top: 50%;
+$excursion_left: 50%;
+.center {
+  width: 400px;
+  height: 400px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  opacity: 0;
+  transition: all 0.5s cubic-bezier(0.445, 0.05, 0, 1);
+  transition-delay: 0s;
+  background-color: rgba(255, 255, 255, 0.13);
+  position: absolute;
+  transform: translate(-50%, -50%);
+  top: $excursion_top;
+  left: $excursion_left;
+  border-radius: 10px;
+  backdrop-filter: blur(10px);
+  border: 2px solid rgba(255, 255, 255, 0.1);
+  box-shadow: 0 0 40px rgba(104, 100, 131, 0.6);
+  padding: 0 35px;
+  z-index: 10;
   .login-form {
     position: relative;
-    width: 520px;
-    max-width: 100%;
-    padding: 160px 35px 0;
-    margin: 0 auto;
+    width: 100%;
     overflow: hidden;
   }
   .tips {
     font-size: 14px;
-    color: #fff;
     margin-bottom: 10px;
-    span {
-      &:first-of-type {
-        margin-right: 16px;
-      }
-    }
   }
   .svg-container {
-    padding: 6px 5px 6px 15px;
+    padding: 0 5px 0 15px;
     color: $dark_gray;
     vertical-align: middle;
     width: 30px;
@@ -249,9 +333,9 @@ $light_gray: #eee;
   .title-container {
     position: relative;
     .title {
-      font-size: 26px;
-      color: $light_gray;
-      margin: 0px auto 40px auto;
+      font-size: 20px;
+      color: $dark_gray;
+      margin: 0px auto 20px auto;
       text-align: center;
       font-weight: bold;
     }
@@ -267,11 +351,16 @@ $light_gray: #eee;
   }
   .veriCodeImg {
     width: 100%;
-    height: 47px;
+    height: 40px;
+    cursor: pointer;
     img {
       width: 100%;
       height: 100%;
     }
+  }
+  .login-btn {
+    width: 100%;
+    margin-bottom: 15px;
   }
 }
 </style>
